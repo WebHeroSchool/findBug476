b300231325f3c25d3dd6ffd4a94fedd38b51d6ff
Elena <lendusik@bk.ru>
2020-08-06 12:52:49 +0300

    comit1

--- bug.js
+++ bug.js
@@ -9,65 +9,65 @@ let cards = document.querySelectorAll(".card");
 let elements = document.querySelectorAll(".list");
 
 const chooseLevel = (elem) => {
-  elements.forEach((item) => item.classList.remove('marker'));
-  elem.target.classList.add('marker');
-}
-elements.forEach((item)=> item.addEventListener('click', chooseLevel));
+  elements.forEach((item) => item.classList.remove("marker"));
+  elem.target.classList.add("marker");
+};
+elements.forEach((item) => item.addEventListener("click", chooseLevel));
 
 let numberOfCards;
 button.onclick = function () {
   mainPage.hidden = true;
   lightLevel.classList.remove("light");
   let currentLevel = document.querySelector(".marker").getAttribute("id");
-  lightLevel.classList.remove('newGrid');
+  lightLevel.classList.remove("newGrid");
   switch (currentLevel) {
-  	case 'light':
+    case "light":
   	  mediumLevel.forEach((element) => {
-      element.classList.add("medium");
-      });
+        element.classList.add("medium");
+        });
       hardLevel.forEach((element) => {
-      element.classList.add("hard");
-	  });
+        element.classList.add("hard");
+	    });
 	  numberOfCards = 3;
-	  break;
-	case 'medium':
+  	  break;
+	case "medium":
 	  mediumLevel.forEach((element) => {
-      element.classList.remove("medium");
-      });
+        element.classList.remove("medium");
+        });
       hardLevel.forEach((element) => {
       element.classList.add("hard");
-	  });
+	    });
       numberOfCards = 6;
 	  break;
-	case 'hard':
+	case "hard":
 	  mediumLevel.forEach((element) => {
-      element.classList.remove("medium");
-      });
+        element.classList.remove("medium");
+        });
 	  hardLevel.forEach((element) => {
-      element.classList.remove("hard");
-	  });
+        element.classList.remove("hard");
+	    });
 	  lightLevel.classList.add('newGrid');
 	  numberOfCards = 10;
 	  break;
-	  }
+  }
 };
 
 let hasFlippedCard = false;
-cards.forEach(card => {
-  card.addEventListener ('click', (event) => {
+cards.forEach((card) => {
+  card.addEventListener ("click", (event) => {
     if(hasFlippedCard) {
 	  mainPage.hidden = false;
 	  lightLevel.classList.add("light");
-	  cards.forEach((item) => item.classList.remove('flip'));
+	  cards.forEach((item) => item.classList.remove("flip"));
 	  cards.forEach((item) => item.lastElementChild.src = "img/finish.png");
 	  hasFlippedCard = false;
  	} else {
-	  card.classList.add('flip');
+	  card.classList.add("flip");
 	  (function mixCards() {
         let randomCard = Math.floor(Math.random() * numberOfCards);
         for (let i = 0; i < numberOfCards; i++ ) {
           if (i === randomCard) {
-            cards[i].lastElementChild.src = "img/bug.png" 
+            cards[i].lastElementChild.src = "img/bug.png";
            };
         };	
       })();
